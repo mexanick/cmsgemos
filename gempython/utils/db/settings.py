bfb40ad48402234a27d3ae4acd7483d2391c5235
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2017-10-16 16:04:36 +0200

    fixing the DB problem

--- gempython/utils/db/settings.py
+++ gempython/utils/db/settings.py
@@ -1,4 +1,7 @@
 import os
+#import sys
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gempython.utils.db.settings.local")
 SECRET_KEY = 'ef7$_)r0$#(x)ovd^m_6_bevsonkaxalvrhds9@ilb_p0o&-xn'
 
@@ -7,11 +10,15 @@ INSTALLED_APPS = [
     ]
 DATABASES = {
     'default': {
-        'ENGINE':  'django.db.backends.mysql',
-        'NAME':     'ldqm_test_db',		
-        'PORT':     3306,		
-        'HOST':     'gem904daq01',		
-        'USER':     'gemdaq',		
-        'PASSWORD': 'gemdaq',
+        'ENGINE':   'django.db.backends.mysql',
+        'NAME':     'ldqm_db',
+        'PORT':     3306,
+        'HOST':     'gemvm-daqcc7.cms',
+        'USER':     'ldqm_dbuser',
+        'PASSWORD': 'clod4_Callow',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(os.getenv('BUILD_HOME'), 'ldqm-browser/LightDQM/LightDQM/lightdqm_db.sqlite3'),
+    #}
 }
